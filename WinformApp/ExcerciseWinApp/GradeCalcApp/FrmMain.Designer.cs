109f00ac34d0b73f4f7b117200897cf60ee32c51
ghd0276 <ghd0276@naver.com>
2021-03-10 17:13:42 +0900

    3/10 활용 150 진행중

---
+++ WinformApp/ExcerciseWinApp/GradeCalcApp/FrmMain.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace GradeCalcApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtSubject1 = new System.Windows.Forms.TextBox();
+            this.TxtSubject2 = new System.Windows.Forms.TextBox();
+            this.TxtSubject3 = new System.Windows.Forms.TextBox();
+            this.TxtSubject4 = new System.Windows.Forms.TextBox();
+            this.TxtSubject5 = new System.Windows.Forms.TextBox();
+            this.TxtSubject6 = new System.Windows.Forms.TextBox();
+            this.TxtSubject7 = new System.Windows.Forms.TextBox();
+            this.CboCredit1 = new System.Windows.Forms.ComboBox();
+            this.CboCredit2 = new System.Windows.Forms.ComboBox();
+            this.CboCredit3 = new System.Windows.Forms.ComboBox();
+            this.CboCredit4 = new System.Windows.Forms.ComboBox();
+            this.CboCredit5 = new System.Windows.Forms.ComboBox();
+            this.CboCredit6 = new System.Windows.Forms.ComboBox();
+            this.CboCredit7 = new System.Windows.Forms.ComboBox();
+            this.CboGrade1 = new System.Windows.Forms.ComboBox();
+            this.CboGrade2 = new System.Windows.Forms.ComboBox();
+            this.CboGrade3 = new System.Windows.Forms.ComboBox();
+            this.CboGrade4 = new System.Windows.Forms.ComboBox();
+            this.CboGrade5 = new System.Windows.Forms.ComboBox();
+            this.CboGrade6 = new System.Windows.Forms.ComboBox();
+            this.CboGrade7 = new System.Windows.Forms.ComboBox();
+            this.BtnAverage = new System.Windows.Forms.Button();
+            this.TxtAverage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "과목명";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "학점";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(412, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "성적";
+            // 
+            // TxtSubject1
+            // 
+            this.TxtSubject1.Location = new System.Drawing.Point(44, 71);
+            this.TxtSubject1.Name = "TxtSubject1";
+            this.TxtSubject1.Size = new System.Drawing.Size(119, 21);
+            this.TxtSubject1.TabIndex = 3;
+            // 
+            // TxtSubject2
+            // 
+            this.TxtSubject2.Location = new System.Drawing.Point(44, 116);
+            this.TxtSubject2.Name = "TxtSubject2";
+            this.TxtSubject2.Size = new System.Drawing.Size(119, 21);
+            this.TxtSubject2.TabIndex = 4;
+            // 
+            // TxtSubject3
+            // 
+            this.TxtSubject3.Location = new System.Drawing.Point(44, 161);
+            this.TxtSubject3.Name = "TxtSubject3";
+            this.TxtSubject3.Size = new System.Drawing.Size(119, 21);
+            this.TxtSubject3.TabIndex = 5;
+            // 
+            // TxtSubject4
+            // 
+            this.TxtSubject4.Location = new System.Drawing.Point(44, 206);
+            this.TxtSubject4.Name = "TxtSubject4";
+            this.TxtSubject4.Size = new System.Drawing.Size(119, 21);
+            this.TxtSubject4.TabIndex = 6;
+            // 
+            // TxtSubject5
+            // 
+            this.TxtSubject5.Location = new System.Drawing.Point(44, 251);
+            this.TxtSubject5.Name = "TxtSubject5";
+            this.TxtSubject5.Size = new System.Drawing.Size(119, 21);
+            this.TxtSubject5.TabIndex = 7;
+            // 
+            // TxtSubject6
+            // 
+            this.TxtSubject6.Location = new System.Drawing.Point(44, 296);
+            this.TxtSubject6.Name = "TxtSubject6";
+            this.TxtSubject6.Size = new System.Drawing.Size(119, 21);
+            this.TxtSubject6.TabIndex = 8;
+            // 
+            // TxtSubject7
+            // 
+            this.TxtSubject7.Location = new System.Drawing.Point(44, 341);
+            this.TxtSubject7.Name = "TxtSubject7";
+            this.TxtSubject7.Size = new System.Drawing.Size(119, 21);
+            this.TxtSubject7.TabIndex = 9;
+            // 
+            // CboCredit1
+            // 
+            this.CboCredit1.FormattingEnabled = true;
+            this.CboCredit1.Location = new System.Drawing.Point(206, 71);
+            this.CboCredit1.Name = "CboCredit1";
+            this.CboCredit1.Size = new System.Drawing.Size(121, 20);
+            this.CboCredit1.TabIndex = 10;
+            // 
+            // CboCredit2
+            // 
+            this.CboCredit2.FormattingEnabled = true;
+            this.CboCredit2.Location = new System.Drawing.Point(206, 117);
+            this.CboCredit2.Name = "CboCredit2";
+            this.CboCredit2.Size = new System.Drawing.Size(121, 20);
+            this.CboCredit2.TabIndex = 11;
+            // 
+            // CboCredit3
+            // 
+            this.CboCredit3.FormattingEnabled = true;
+            this.CboCredit3.Location = new System.Drawing.Point(206, 161);
+            this.CboCredit3.Name = "CboCredit3";
+            this.CboCredit3.Size = new System.Drawing.Size(121, 20);
+            this.CboCredit3.TabIndex = 12;
+            // 
+            // CboCredit4
+            // 
+            this.CboCredit4.FormattingEnabled = true;
+            this.CboCredit4.Location = new System.Drawing.Point(206, 206);
+            this.CboCredit4.Name = "CboCredit4";
+            this.CboCredit4.Size = new System.Drawing.Size(121, 20);
+            this.CboCredit4.TabIndex = 13;
+            // 
+            // CboCredit5
+            // 
+            this.CboCredit5.FormattingEnabled = true;
+            this.CboCredit5.Location = new System.Drawing.Point(206, 251);
+            this.CboCredit5.Name = "CboCredit5";
+            this.CboCredit5.Size = new System.Drawing.Size(121, 20);
+            this.CboCredit5.TabIndex = 14;
+            // 
+            // CboCredit6
+            // 
+            this.CboCredit6.FormattingEnabled = true;
+            this.CboCredit6.Location = new System.Drawing.Point(206, 296);
+            this.CboCredit6.Name = "CboCredit6";
+            this.CboCredit6.Size = new System.Drawing.Size(121, 20);
+            this.CboCredit6.TabIndex = 15;
+            // 
+            // CboCredit7
+            // 
+            this.CboCredit7.FormattingEnabled = true;
+            this.CboCredit7.Location = new System.Drawing.Point(206, 341);
+            this.CboCredit7.Name = "CboCredit7";
+            this.CboCredit7.Size = new System.Drawing.Size(121, 20);
+            this.CboCredit7.TabIndex = 16;
+            // 
+            // CboGrade1
+            // 
+            this.CboGrade1.FormattingEnabled = true;
+            this.CboGrade1.Location = new System.Drawing.Point(369, 71);
+            this.CboGrade1.Name = "CboGrade1";
+            this.CboGrade1.Size = new System.Drawing.Size(121, 20);
+            this.CboGrade1.TabIndex = 17;
+            // 
+            // CboGrade2
+            // 
+            this.CboGrade2.FormattingEnabled = true;
+            this.CboGrade2.Location = new System.Drawing.Point(369, 117);
+            this.CboGrade2.Name = "CboGrade2";
+            this.CboGrade2.Size = new System.Drawing.Size(121, 20);
+            this.CboGrade2.TabIndex = 18;
+            // 
+            // CboGrade3
+            // 
+            this.CboGrade3.FormattingEnabled = true;
+            this.CboGrade3.Location = new System.Drawing.Point(369, 162);
+            this.CboGrade3.Name = "CboGrade3";
+            this.CboGrade3.Size = new System.Drawing.Size(121, 20);
+            this.CboGrade3.TabIndex = 19;
+            // 
+            // CboGrade4
+            // 
+            this.CboGrade4.FormattingEnabled = true;
+            this.CboGrade4.Location = new System.Drawing.Point(369, 207);
+            this.CboGrade4.Name = "CboGrade4";
+            this.CboGrade4.Size = new System.Drawing.Size(121, 20);
+            this.CboGrade4.TabIndex = 20;
+            // 
+            // CboGrade5
+            // 
+            this.CboGrade5.FormattingEnabled = true;
+            this.CboGrade5.Location = new System.Drawing.Point(369, 252);
+            this.CboGrade5.Name = "CboGrade5";
+            this.CboGrade5.Size = new System.Drawing.Size(121, 20);
+            this.CboGrade5.TabIndex = 21;
+            // 
+            // CboGrade6
+            // 
+            this.CboGrade6.FormattingEnabled = true;
+            this.CboGrade6.Location = new System.Drawing.Point(369, 297);
+            this.CboGrade6.Name = "CboGrade6";
+            this.CboGrade6.Size = new System.Drawing.Size(121, 20);
+            this.CboGrade6.TabIndex = 22;
+            // 
+            // CboGrade7
+            // 
+            this.CboGrade7.FormattingEnabled = true;
+            this.CboGrade7.Location = new System.Drawing.Point(369, 342);
+            this.CboGrade7.Name = "CboGrade7";
+            this.CboGrade7.Size = new System.Drawing.Size(121, 20);
+            this.CboGrade7.TabIndex = 23;
+            // 
+            // BtnAverage
+            // 
+            this.BtnAverage.Location = new System.Drawing.Point(206, 385);
+            this.BtnAverage.Name = "BtnAverage";
+            this.BtnAverage.Size = new System.Drawing.Size(121, 30);
+            this.BtnAverage.TabIndex = 24;
+            this.BtnAverage.Text = "평균평점";
+            this.BtnAverage.UseVisualStyleBackColor = true;
+            // 
+            // TxtAverage
+            // 
+            this.TxtAverage.Location = new System.Drawing.Point(369, 391);
+            this.TxtAverage.Name = "TxtAverage";
+            this.TxtAverage.Size = new System.Drawing.Size(121, 21);
+            this.TxtAverage.TabIndex = 25;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 446);
+            this.Controls.Add(this.TxtAverage);
+            this.Controls.Add(this.BtnAverage);
+            this.Controls.Add(this.CboGrade7);
+            this.Controls.Add(this.CboGrade6);
+            this.Controls.Add(this.CboGrade5);
+            this.Controls.Add(this.CboGrade4);
+            this.Controls.Add(this.CboGrade3);
+            this.Controls.Add(this.CboGrade2);
+            this.Controls.Add(this.CboGrade1);
+            this.Controls.Add(this.CboCredit7);
+            this.Controls.Add(this.CboCredit6);
+            this.Controls.Add(this.CboCredit5);
+            this.Controls.Add(this.CboCredit4);
+            this.Controls.Add(this.CboCredit3);
+            this.Controls.Add(this.CboCredit2);
+            this.Controls.Add(this.CboCredit1);
+            this.Controls.Add(this.TxtSubject7);
+            this.Controls.Add(this.TxtSubject6);
+            this.Controls.Add(this.TxtSubject5);
+            this.Controls.Add(this.TxtSubject4);
+            this.Controls.Add(this.TxtSubject3);
+            this.Controls.Add(this.TxtSubject2);
+            this.Controls.Add(this.TxtSubject1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "학점계산기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtSubject1;
+        private System.Windows.Forms.TextBox TxtSubject2;
+        private System.Windows.Forms.TextBox TxtSubject3;
+        private System.Windows.Forms.TextBox TxtSubject4;
+        private System.Windows.Forms.TextBox TxtSubject5;
+        private System.Windows.Forms.TextBox TxtSubject6;
+        private System.Windows.Forms.TextBox TxtSubject7;
+        private System.Windows.Forms.ComboBox CboCredit1;
+        private System.Windows.Forms.ComboBox CboCredit2;
+        private System.Windows.Forms.ComboBox CboCredit3;
+        private System.Windows.Forms.ComboBox CboCredit4;
+        private System.Windows.Forms.ComboBox CboCredit5;
+        private System.Windows.Forms.ComboBox CboCredit6;
+        private System.Windows.Forms.ComboBox CboCredit7;
+        private System.Windows.Forms.ComboBox CboGrade1;
+        private System.Windows.Forms.ComboBox CboGrade2;
+        private System.Windows.Forms.ComboBox CboGrade3;
+        private System.Windows.Forms.ComboBox CboGrade4;
+        private System.Windows.Forms.ComboBox CboGrade5;
+        private System.Windows.Forms.ComboBox CboGrade6;
+        private System.Windows.Forms.ComboBox CboGrade7;
+        private System.Windows.Forms.Button BtnAverage;
+        private System.Windows.Forms.TextBox TxtAverage;
+    }
+}
+
---
+++ WinformApp/ExcerciseWinApp/ListViewApp/FrmMain.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace ListViewApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.RdbDetails = new System.Windows.Forms.RadioButton();
+            this.RdbList = new System.Windows.Forms.RadioButton();
+            this.RdbSmallIcon = new System.Windows.Forms.RadioButton();
+            this.RdbBigIcon = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtSelected = new System.Windows.Forms.TextBox();
+            this.LsvProduct = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ImgSmallIcon = new System.Windows.Forms.ImageList(this.components);
+            this.ImgLargeIcon = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // RdbDetails
+            // 
+            this.RdbDetails.AutoSize = true;
+            this.RdbDetails.Location = new System.Drawing.Point(49, 25);
+            this.RdbDetails.Name = "RdbDetails";
+            this.RdbDetails.Size = new System.Drawing.Size(59, 16);
+            this.RdbDetails.TabIndex = 0;
+            this.RdbDetails.TabStop = true;
+            this.RdbDetails.Text = "자세히";
+            this.RdbDetails.UseVisualStyleBackColor = true;
+            this.RdbDetails.CheckedChanged += new System.EventHandler(this.RdbDetails_CheckedChanged);
+            // 
+            // RdbList
+            // 
+            this.RdbList.AutoSize = true;
+            this.RdbList.Location = new System.Drawing.Point(159, 25);
+            this.RdbList.Name = "RdbList";
+            this.RdbList.Size = new System.Drawing.Size(59, 16);
+            this.RdbList.TabIndex = 1;
+            this.RdbList.TabStop = true;
+            this.RdbList.Text = "리스트";
+            this.RdbList.UseVisualStyleBackColor = true;
+            this.RdbList.CheckedChanged += new System.EventHandler(this.RdbList_CheckedChanged);
+            // 
+            // RdbSmallIcon
+            // 
+            this.RdbSmallIcon.AutoSize = true;
+            this.RdbSmallIcon.Location = new System.Drawing.Point(269, 25);
+            this.RdbSmallIcon.Name = "RdbSmallIcon";
+            this.RdbSmallIcon.Size = new System.Drawing.Size(87, 16);
+            this.RdbSmallIcon.TabIndex = 2;
+            this.RdbSmallIcon.TabStop = true;
+            this.RdbSmallIcon.Text = "작은 아이콘";
+            this.RdbSmallIcon.UseVisualStyleBackColor = true;
+            this.RdbSmallIcon.CheckedChanged += new System.EventHandler(this.RdbSmallIcon_CheckedChanged);
+            // 
+            // RdbBigIcon
+            // 
+            this.RdbBigIcon.AutoSize = true;
+            this.RdbBigIcon.Location = new System.Drawing.Point(407, 25);
+            this.RdbBigIcon.Name = "RdbBigIcon";
+            this.RdbBigIcon.Size = new System.Drawing.Size(75, 16);
+            this.RdbBigIcon.TabIndex = 3;
+            this.RdbBigIcon.TabStop = true;
+            this.RdbBigIcon.Text = "큰 아이콘";
+            this.RdbBigIcon.UseVisualStyleBackColor = true;
+            this.RdbBigIcon.CheckedChanged += new System.EventHandler(this.RdbBigIcon_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Selected : ";
+            // 
+            // TxtSelected
+            // 
+            this.TxtSelected.Location = new System.Drawing.Point(229, 272);
+            this.TxtSelected.Name = "TxtSelected";
+            this.TxtSelected.Size = new System.Drawing.Size(288, 21);
+            this.TxtSelected.TabIndex = 6;
+            // 
+            // LsvProduct
+            // 
+            this.LsvProduct.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.LsvProduct.FullRowSelect = true;
+            this.LsvProduct.GridLines = true;
+            this.LsvProduct.HideSelection = false;
+            this.LsvProduct.LargeImageList = this.ImgLargeIcon;
+            this.LsvProduct.Location = new System.Drawing.Point(12, 47);
+            this.LsvProduct.Name = "LsvProduct";
+            this.LsvProduct.Size = new System.Drawing.Size(505, 210);
+            this.LsvProduct.SmallImageList = this.ImgSmallIcon;
+            this.LsvProduct.TabIndex = 7;
+            this.LsvProduct.UseCompatibleStateImageBehavior = false;
+            this.LsvProduct.View = System.Windows.Forms.View.Details;
+            this.LsvProduct.SelectedIndexChanged += new System.EventHandler(this.LsvProduct_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "제품명";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "단가";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "수량";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader3.Width = 90;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "금액";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader4.Width = 105;
+            // 
+            // ImgSmallIcon
+            // 
+            this.ImgSmallIcon.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ImgSmallIcon.ImageStream")));
+            this.ImgSmallIcon.TransparentColor = System.Drawing.Color.Transparent;
+            this.ImgSmallIcon.Images.SetKeyName(0, "controller.png");
+            this.ImgSmallIcon.Images.SetKeyName(1, "ds.png");
+            this.ImgSmallIcon.Images.SetKeyName(2, "ps4.png");
+            this.ImgSmallIcon.Images.SetKeyName(3, "remote.png");
+            this.ImgSmallIcon.Images.SetKeyName(4, "xbox.png");
+            // 
+            // ImgLargeIcon
+            // 
+            this.ImgLargeIcon.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ImgLargeIcon.ImageStream")));
+            this.ImgLargeIcon.TransparentColor = System.Drawing.Color.Transparent;
+            this.ImgLargeIcon.Images.SetKeyName(0, "controller.png");
+            this.ImgLargeIcon.Images.SetKeyName(1, "ds.png");
+            this.ImgLargeIcon.Images.SetKeyName(2, "ps4.png");
+            this.ImgLargeIcon.Images.SetKeyName(3, "remote.png");
+            this.ImgLargeIcon.Images.SetKeyName(4, "xbox.png");
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 310);
+            this.Controls.Add(this.LsvProduct);
+            this.Controls.Add(this.TxtSelected);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RdbBigIcon);
+            this.Controls.Add(this.RdbSmallIcon);
+            this.Controls.Add(this.RdbList);
+            this.Controls.Add(this.RdbDetails);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "상품리스트";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton RdbDetails;
+        private System.Windows.Forms.RadioButton RdbList;
+        private System.Windows.Forms.RadioButton RdbSmallIcon;
+        private System.Windows.Forms.RadioButton RdbBigIcon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtSelected;
+        private System.Windows.Forms.ListView LsvProduct;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ImageList ImgSmallIcon;
+        private System.Windows.Forms.ImageList ImgLargeIcon;
+    }
+}
+
