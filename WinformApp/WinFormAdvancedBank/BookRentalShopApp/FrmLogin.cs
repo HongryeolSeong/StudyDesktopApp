ad0b97b2203245db74bc13cfb5dc64874c8365d4
ghd0276 <ghd0276@naver.com>
2021-03-16 17:28:00 +0900

    3/16 sqlreader 까지

---
+++ WinformApp/WinFormAdvancedBank/BookRentalShopApp/FrmLogin.cs
@@ -0,0 +1,81 @@
+﻿using MetroFramework;
+using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookRentalShopApp
+{
+    public partial class FrmLogin : MetroForm
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("로그인 처리!");
+            if (string.IsNullOrEmpty(TxtUserId.Text) || string.IsNullOrEmpty(TxtPassword.Text))
+            {
+                MetroMessageBox.Show(this, "아이디/패스워드를 입력하세요!", "오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                // SqlConnection 연결
+                using (SqlConnection conn = new SqlConnection(Helper.Common.ConnString))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+
+                    // SqlCommand 생성
+                    SqlCommand cmd = new SqlCommand();
+
+                    // SQLInjection 해킹 막기위해서 사용
+                    SqlParameter param;
+
+                    // SqlDataReader 실행(1) 쿼리와 다른 방법
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // reader로 처리....
+                }
+            }
+            catch (Exception ex)
+            {
+                MetroMessageBox.Show(this, $"Error : {ex.Message}", "오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+
+        private void TxtUserId_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) TxtPassword.Focus();
+        }
+
+        private void TxtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) BtnLogin_Click(sender, e);
+        }
+    }
+}
---
+++ WinformApp/WinFormAdvancedBank/BookRentalShopApp/Helper/Common.cs
@@ -0,0 +1,13 @@
+﻿namespace BookRentalShopApp.Helper
+{
+    public static class Common
+    {
+        public static string ConnString = "Data Source=127.0.0.1; " +
+                                          "Initial Catalog=bookrentalshop; " +
+                                          "Persist Security Info=True; " +
+                                          "User ID=sa; " +
+                                          "Password=mssql_p@ssw0rd!";
+
+        public static string LoginUserId = string.Empty;
+    }
+}
