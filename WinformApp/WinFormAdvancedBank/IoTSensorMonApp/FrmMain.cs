e8c2102383cc403e2bc014fd2adea5d2450ac3d5
ghd0276 <ghd0276@naver.com>
2021-03-16 15:02:21 +0900

    3/16 아두이노 완료

--- WinformApp/WinFormAdvancedBank/IoTSensorMonApp/FrmMain.cs
+++ WinformApp/WinFormAdvancedBank/IoTSensorMonApp/FrmMain.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO.Ports;
 using System.Linq;
@@ -14,7 +16,17 @@ namespace IoTSensorMonApp
 {
     public partial class FrmMain : Form
     {
-        private double xCount = 200; // 차트에 보이는 
+        private double xCount = 200; // 차트에 보이는 데이터 수
+        private Timer timerSimul = new Timer();
+        private Random randPhoto = new Random();
+        private bool IsSimulation = false;
+        private List<SensorData> sensors = new List<SensorData>(); // 차트, 리스트박스에 계속 출력될 데이터
+        private string connString = "Data Source=127.0.0.1;" +
+                                    "Initial Catalog=IoTData;" +
+                                    "Persist Security Info=True;" +
+                                    "User ID=sa;" +
+                                    "PassWord=mssql_p@ssw0rd!";
+
         public FrmMain()
         {
             InitializeComponent();
@@ -101,10 +113,7 @@ namespace IoTSensorMonApp
             // TODO 나중에 실제 작업시 작성
         }
 
-        private Timer timerSimul = new Timer();
-        private Random randPhoto = new Random();
-        private bool IsSimulation = false;
-        private List<SensorData> sensors = new List<SensorData>(); // 차트, 리스트박스에 계속 출력될 데이터
+        
 
         /// <summary>
         /// 시뮬레이션 시작
@@ -137,6 +146,7 @@ namespace IoTSensorMonApp
             var currentTime = DateTime.Now;
             SensorData data = new SensorData(currentTime, v, IsSimulation);
             sensors.Add(data);
+            InsertTable(data);
 
             // 센서값 갯수
             TxtSensorNum.Text = sensors.Count.ToString(); // or $"{sensors.Count}";
@@ -149,6 +159,58 @@ namespace IoTSensorMonApp
 
             // 차트에 데이터 출력
             ChtPhotoResistors.Series[0].Points.Add(v);
+
+            // 200개 넘으면
+            ChtPhotoResistors.ChartAreas[0].AxisX.Minimum = 0;
+            ChtPhotoResistors.ChartAreas[0].AxisX.Maximum = (sensors.Count >= xCount) ? sensors.Count : xCount;
+
+            // Zoom처리
+            if (sensors.Count > xCount)
+            {
+                ChtPhotoResistors.ChartAreas[0].AxisX.ScaleView.Zoom(sensors.Count - xCount, sensors.Count);
+            }
+            else
+            {
+                ChtPhotoResistors.ChartAreas[0].AxisX.ScaleView.Zoom(0, xCount);
+            }
+
+            // BtnDisplay
+            if (IsSimulation)
+            {
+                BtnDisplay.Text = v.ToString();
+            }
+            else
+            {
+                BtnDisplay.Text = "~" + "\n" + v.ToString();
+            }
+        }
+
+        /// <summary>
+        /// IoTData 데이터베이스 내 Tbl_PhotoResistor 테이블에 센서데이터 입력
+        /// </summary>
+        /// <param name="data"></param>
+        private void InsertTable(SensorData data)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                    }
+                    var query = $"insert into Tbl_PhotoResistor " +
+                        $"(CurrentDt, Value, SimulFlag) " +
+                        $"values " +
+                        $"('{data.Current.ToString("yyyy-MM-dd HH:mm:ss")}', '{data.Value}', '{(data.SimulFlag == true ? "1" : "0")}');";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"예외발생 : {ex.Message}");
+            }
         }
 
         /// <summary>
@@ -177,6 +239,24 @@ namespace IoTSensorMonApp
             }
 
             Environment.Exit(0);
+        }
+
+        private void BtnViewAll_Click(object sender, EventArgs e)
+        {
+            ChtPhotoResistors.ChartAreas[0].AxisX.Minimum = 0;
+            ChtPhotoResistors.ChartAreas[0].AxisX.Maximum = sensors.Count;
+
+            ChtPhotoResistors.ChartAreas[0].AxisX.ScaleView.Zoom(0, sensors.Count);
+            ChtPhotoResistors.ChartAreas[0].AxisX.Interval = sensors.Count / 4;
+        }
+
+        private void BtnZoom_Click(object sender, EventArgs e)
+        {
+            ChtPhotoResistors.ChartAreas[0].AxisX.Minimum = 0;
+            ChtPhotoResistors.ChartAreas[0].AxisX.Maximum = sensors.Count;
+
+            ChtPhotoResistors.ChartAreas[0].AxisX.ScaleView.Zoom(sensors.Count - xCount, sensors.Count);
+            ChtPhotoResistors.ChartAreas[0].AxisX.Interval = xCount / 4;
         }
     }
 }
