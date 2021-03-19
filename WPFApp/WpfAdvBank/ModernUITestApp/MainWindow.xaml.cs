dbe0cdf5e9fe316f0266fd4d2808b5a1a359ee04
ghd0276 <ghd0276@naver.com>
2021-03-19 17:28:57 +0900

    3/19 wpf 기본

--- WPFApp/WpfAdvBank/ModernUITestApp/MainWindow.xaml.cs
+++ WPFApp/WpfAdvBank/ModernUITestApp/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Data;
 
 namespace ModernUITestApp
 {
@@ -24,6 +26,37 @@ namespace ModernUITestApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://www.youtube.com");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MnuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MetroWindow_Initialized(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("VALUE", typeof(string));
+            dt.Columns.Add("NAME", typeof(string));
+
+            dt.Rows.Add(new string[] { "B001", "공포/스릴러" });
+            dt.Rows.Add(new string[] { "B002", "로맨스" });
+            dt.Rows.Add(new string[] { "B003", "SF" });
+            dt.Rows.Add(new string[] { "B004", "무협" });
+
+            CboDivision.ItemsSource = dt.DefaultView;
+            CboDivision.DisplayMemberPath = "NAME";
+            CboDivision.SelectedValuePath = "VALUE";
         }
     }
 }
