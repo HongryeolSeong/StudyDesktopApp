fc0087dd6d4d214752180dc616b8b8767e64c5d5
ghd0276 <ghd0276@naver.com>
2021-03-23 12:31:45 +0900

    3/23 점심전 커밋

--- WPFApp/WpfAdvBank/PhotoSensorMonApp/MainWindow.xaml.cs
+++ WPFApp/WpfAdvBank/PhotoSensorMonApp/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using MahApps.Metro.Controls;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PhotoSensorMonApp
 {
@@ -26,11 +30,30 @@ namespace PhotoSensorMonApp
             InitializeComponent();
         }
 
+        public ChartValues<float> ChartValues { get; set; }
+        public double SensorValue { get; set; }
+        public DispatcherTimer CustomTimer { get; set; }
+
+
         private void Window_Initialized(object sender, EventArgs e)
         {
             /*var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
             var y = x.Select(v => Math.Abs(v) < 1e-10 ? 1 : Math.Sin(v) / v);
             ChtLine.Plot(x, y);*/
+
+            ChartValues = new ChartValues<float>() { 4, 1, 5, 2, 6, 3.9f };
+            GrdHistory.DataContext = ChartValues;
+
+            CustomTimer = new DispatcherTimer();
+            CustomTimer.Interval = TimeSpan.FromSeconds(1);
+            CustomTimer.Tick += CustomTimer_Tick;
+            CustomTimer.Start();
+        }
+
+        private void CustomTimer_Tick(object sender, EventArgs e)
+        {
+            SensorValue = new Random().Next(0, 1023);
+            GrdRealTime.DataContext = SensorValue;
         }
     }
 }
